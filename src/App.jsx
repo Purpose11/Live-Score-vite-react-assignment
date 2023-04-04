9cfa10a6d78829c80168f0ceb1cb7858a0573ffa
Purpose11 <adebayofatai924@gmail.com>
2023-04-04 18:38:54 +0100

    file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import chelseaImage from './images/chelsea.png';
 import ManUImage from './images/manu.png';
 import './App.css'
@@ -6,19 +6,30 @@ import './App.css'
 function App() {
   const [ChelseaScores, setChelseaScore] = useState(0);
   const [ManUScores, setManUScores] = useState(0);
-  
+  const [scale, setScale] = useState(1);
+
+  useEffect(() => {
+    if (ManUScores > ChelseaScores) {
+      setScale(1.5);
+    } else if (ManUScores === ChelseaScores) {
+      setScale(1);
+    } else {
+      setScale(1.5);
+    }
+  }, [ChelseaScores, ManUScores]);
+
   return (
     <div className="App">
       <h1>Live Scores</h1>
       <div className='container'>
           <div className='club-logo'>
-          <img src={chelseaImage} alt="Chelsea FC"/>
+          <img src={chelseaImage}  style={{ transform:  `scale(${ChelseaScores >= ManUScores ? scale : 1})` }}alt="Chelsea FC"/>
           <p>Chelsea FC</p>
           <p className='score'>{ChelseaScores}</p>
           </div>
               <h2>VS</h2>
           <div className='club-logo'>
-          <img src={ManUImage} alt="Manchester United" />
+          <img src={ManUImage} style={{ transform:  `scale(${ManUScores >= ChelseaScores ? scale : 1})` }} alt="Manchester United" />
           <p>Manchester United</p>
           <p className='score'>{ManUScores}</p>
           </div>
@@ -37,4 +48,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
